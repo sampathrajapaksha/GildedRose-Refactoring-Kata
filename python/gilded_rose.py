2808d119a78acc68fe56660d64522c905ae9183b
sampathrajapaksha <sasu.uop@gmail.com>
2025-12-08 23:02:40 +0000

    Introduce strategy classes for item wise update logic

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,4 +1,54 @@
 # -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
+
+MAX_QUALITY = 50
+MIN_QUALITY = 0
+
+class UpdateStrategy(ABC):
+    @abstractmethod
+    def update_item(self, item):
+        pass
+
+class NormalItems(UpdateStrategy):
+    def update_item(self, item):
+        if item.sell_in > 0:
+            # degrade by 1
+            item.quality = max(item.quality - 1, MIN_QUALITY)
+        else:
+            # degrade twice
+            item.quality = max(item.quality - 2, MIN_QUALITY)
+        item.sell_in -= 1
+
+class AgedBrie(UpdateStrategy):
+    def update_item(self, item):
+        if item.sell_in > 0:
+            # increase by 1
+            item.quality = min(item.quality + 1, MAX_QUALITY)
+        else:
+            # increase twice
+            item.quality = min(item.quality + 2, MAX_QUALITY)
+        item.sell_in -= 1
+
+class BackstagePasses(UpdateStrategy):
+    def update_item(self, item):
+        if item.sell_in <= 0:
+           item.quality = 0
+        elif item.sell_in <= 5:
+            # increase by 3
+            item.quality = min(item.quality + 3, MAX_QUALITY)
+        elif item.sell_in <= 10:
+            # increase by 2
+            item.quality = min(item.quality + 2, MAX_QUALITY)
+        else:
+            # increase by 1
+            item.quality = min(item.quality + 1, MAX_QUALITY)
+        item.sell_in -= 1
+
+class Sulfuras(UpdateStrategy):
+    def update_item(self, item):
+        # No changes for Sulfuras
+        pass
+
 
 class GildedRose(object):
 
